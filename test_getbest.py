eaec200d3f6668bd85f1b4ae86afa94b9cb5022b
Caseiin <leruopono@gmail.com>
2026-04-17 13:28:56 +0200

    test_getbest| getcolumn mark test

--- test_getbest.py
+++ test_getbest.py
@@ -17,6 +17,11 @@ class TestGetBest(unittest.TestCase):
         num_col, _ = getCols(f)
         self.assertEqual(num_col,1)
 
-    def test_getCols_returns_correct_mark_column():
-        
+    def test_getCols_returns_correct_mark_column(self):
+        f = make_file("Course,Student Number,Mark,Comment")
+        _, mark_col = getCols(f)
+        self.assertEqual(mark_col,2)
+
+    
+
 
