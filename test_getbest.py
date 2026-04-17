e3588abe676efaa32cc3cf4965c02b54079fefc4
Caseiin <leruopono@gmail.com>
2026-04-17 13:27:07 +0200

    test_getBest| test column for student number

--- test_getbest.py
+++ test_getbest.py
@@ -1,1 +1,22 @@
 import unittest;
+import io;
+from getbest import getCols, findTop
+
+#helper function used to create fake files using stringIO
+
+def make_file(*lines):
+    return io.StringIO("\n" .join(lines) + "\n")
+
+
+#class
+class TestGetBest(unittest.TestCase):
+    # Tests for getCols
+
+    def test_getCols_return_correct_student_number_column(self):
+        f = make_file("Course,Student Number,Mark,Comment")
+        num_col, _ = getCols(f)
+        self.assertEqual(num_col,1)
+
+    def test_getCols_returns_correct_mark_column():
+        
+
